dba078064aef89ec7f4d4bc59b206404d8f952aa
Akila-Piumal <akilapiumal16@gmail.com>
2022-07-12 09:59:18 +0530

    navigation aded

--- src/controller/ManageStudentsFormController.java
+++ src/controller/ManageStudentsFormController.java
@@ -1,4 +1,41 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
+
 public class ManageStudentsFormController {
+    public AnchorPane dashBoardContext;
+
+    public void saveStudentFormOnAction(ActionEvent actionEvent) throws IOException {
+        setUI("saveStudentForm");
+    }
+
+    public void updateStudentFormOnAction(ActionEvent actionEvent) throws IOException {
+        setUI("updateStudentForm");
+    }
+
+    public void deleteStudentFormOnAction(ActionEvent actionEvent) throws IOException {
+        setUI("deleteStudentForm");
+    }
+
+    public void searchStudentFormOnAction(ActionEvent actionEvent) throws IOException {
+        setUI("searchStudentForm");
+    }
+
+    public void allStudentsFormOnAction(ActionEvent actionEvent) throws IOException {
+        setUI("allStudentsForm");
+    }
+
+    private void setUI(String URI) throws IOException {
+        Stage stage = (Stage) dashBoardContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../views/"+URI+".fxml"))));
+        stage.setTitle(URI);
+        stage.show();
+    }
 }
