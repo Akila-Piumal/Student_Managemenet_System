c94a844b19ae44ec50f449e3c7cc6da586f80a7a
Akila-Piumal <akilapiumal16@gmail.com>
2022-07-12 11:49:36 +0530

    btnSearchOnAction, backToHomeOnAction, txtSearchOnAction methods Refactored

--- src/controller/SearchStudentFormController.java
+++ src/controller/SearchStudentFormController.java
@@ -1,7 +1,16 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class SearchStudentFormController {
     public TextField txtId;
@@ -10,12 +19,49 @@ public class SearchStudentFormController {
     public TextField txtContact;
     public TextField txtAddress;
     public TextField txtNic;
+    public AnchorPane searchStudentFormContext;
 
     public void btnSearchOnAction(ActionEvent actionEvent) {
-
+        try {
+            ResultSet resultSet = StudentCrudController.searchStudent(txtId.getText());
+            if (resultSet.next()){
+                txtName.setText(resultSet.getString(2));
+                txtEmail.setText(resultSet.getString(3));
+                txtContact.setText(resultSet.getString(4));
+                txtAddress.setText(resultSet.getString(5));
+                txtNic.setText(resultSet.getString(6));
+            }else {
+                new Alert(Alert.AlertType.WARNING,"Invalid student Id").show();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void backToHomeOnAction(ActionEvent actionEvent) {
+    public void backToHomeOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) searchStudentFormContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../views/manageStudentsForm.fxml"))));
+        stage.show();
+    }
 
+    public void txtSerachOnAction(ActionEvent actionEvent) {
+        try {
+            ResultSet resultSet = StudentCrudController.searchStudent(txtId.getText());
+            if (resultSet.next()){
+                txtName.setText(resultSet.getString(2));
+                txtEmail.setText(resultSet.getString(3));
+                txtContact.setText(resultSet.getString(4));
+                txtAddress.setText(resultSet.getString(5));
+                txtNic.setText(resultSet.getString(6));
+            }else {
+                new Alert(Alert.AlertType.WARNING,"Invalid student Id").show();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
