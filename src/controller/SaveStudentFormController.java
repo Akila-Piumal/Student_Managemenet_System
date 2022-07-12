ebdf4c83c9aee08b4a9d73ca7afb1d38d0b1f66c
Akila-Piumal <akilapiumal16@gmail.com>
2022-07-12 11:26:14 +0530

    btnSaveOnAction method refactored and backToHomeOnAction method refactored

--- src/controller/SaveStudentFormController.java
+++ src/controller/SaveStudentFormController.java
@@ -1,8 +1,18 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import model.Student;
+
+import java.io.IOException;
+import java.sql.SQLException;
 
 public class SaveStudentFormController {
     public TextField txtId;
@@ -11,12 +21,25 @@ public class SaveStudentFormController {
     public TextField txtContact;
     public TextField txtAddress;
     public TextField txtNic;
+    public AnchorPane saveStudentFormContext;
 
     public void btnSaveOnAction(ActionEvent actionEvent) {
-
+        try {
+            if (CustomerCrudController.saveStudent(new Student(txtId.getText(),txtName.getText(),txtEmail.getText(),txtContact.getText(),txtAddress.getText(),txtNic.getText()))) {
+                new Alert(Alert.AlertType.CONFIRMATION,"Saved").show();
+            }else{
+                new Alert(Alert.AlertType.WARNING,"Something went Wrong").show();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void backToHomeOnAction(ActionEvent actionEvent) {
-
+    public void backToHomeOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) saveStudentFormContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../views/manageStudentsForm.fxml"))));
+        stage.show();
     }
 }
--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -10,7 +10,7 @@ public class DBConnection {
     private Connection connection;
 
     private DBConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
+        Class.forName("com.mysql.jdbc.Driver");
         connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/student_management","root","1234");
     }
 
